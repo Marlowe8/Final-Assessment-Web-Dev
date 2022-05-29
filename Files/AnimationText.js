d8de989fb7f7077a1ffcb77ec6d52a54c824b54f
Marlowe8 <91404730+Marlowe8@users.noreply.github.com>
2022-05-29 17:52:46 +0400

    Add files via upload

---
+++ Files/AnimationText.js
@@ -0,0 +1,14 @@
+﻿var typed = new Typed(".auto-type", {
+    strings: ["You", "Health", "Life"],
+    typeSpeed: 500,
+    backSpeed: 150,
+    loop: true
+})
+
+function imageFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
